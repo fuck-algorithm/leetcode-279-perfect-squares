5a66074361d256f0a33b9f1828deed9663627d93
songzhibin97 <718428482@qq.com>
2026-02-28 11:43:22 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [react()],
+    base: '/leetcode-279-perfect-squares/',
     server: {
         port: 15674,
     },
